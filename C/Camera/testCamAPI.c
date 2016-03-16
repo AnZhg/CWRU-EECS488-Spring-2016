8296ef7a1ad3d9ec2d57fd054e117a038ad2ed0d
AnZhg <anzhg@icloud.com>
2016-03-16 18:20:28 -0400

    Modify C code for using four PiCam.

--- C/Camera/testCamAPI.c
+++ C/Camera/testCamAPI.c
@@ -13,7 +13,11 @@ int main(int argc, const char *argv[])
 {
 	camPic5M(1, NULL);
 	camPic2M(2, "CAM2_2M.jpg");
-	camVid1080P(1, 3, 30, "CAM1_1080P.h264");
+	camPic320x240(3, "CAM3_Low.jpg");
+    camPic1M(4, "CAM4_Mid.jpg");
+    camVid1080P(1, 3, 30, "CAM1_1080P.h264");
 	camVidQVGA(2, 9.6, 20, NULL);
-	return 0;
+	camVid720P(3, 10, 5, "CAM3_720P.h264");
+    camVid480P(4, 10, 10, "CAM4_480P.h264");
+    return 0;
 }
